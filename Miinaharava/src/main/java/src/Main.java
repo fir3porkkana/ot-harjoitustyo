bbef80a17c5b8106c8b4ee67dc3da0ecdd41028e
Salmi Tiera V E <tiera@dx5-cs-bk107-25.ad.helsinki.fi>
2019-04-02 18:57:50 +0300

    jotain tehtynä

--- Miinaharava/src/main/java/src/Main.java
+++ Miinaharava/src/main/java/src/Main.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package src; 
+package src;
 
 /**
  *
  * @author tiera
  */
 public class Main implements Runnable {
-    
+
     GUI gui = new GUI();
-  
+
     public static void main(String[] args) {
         new Thread(new Main()).start();
     }
@@ -20,8 +20,13 @@ public class Main implements Runnable {
     @Override
     public void run() {
         while (true) {
-            gui.repaint();
+            try {
+                gui.repaint();
+                Thread.sleep(20);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
-    
+
 }
